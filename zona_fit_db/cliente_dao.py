c16c8629b4b4be0d58025389fbc4fdbf94c29c40
Danny <dannyapaza40@gmail.com>
2026-01-12 13:20:38 -0500

    Eliminar

--- zona_fit_db/cliente_dao.py
+++ zona_fit_db/cliente_dao.py
@@ -46,12 +46,56 @@ class ClienteDAO:
                 cursor.close()
                 Conexion.liberar_conexion(conexion)
 
+    @classmethod
+    def actualizar(cls, cliente):
+        conexion = None
+        try:
+            conexion = Conexion().obtener_conexion()
+            cursor = conexion.cursor()
+            valores = (cliente.nombre, cliente.apellido, cliente.membresia, cliente.id)
+            cursor.execute(cls.ACTUALIZAR, valores)
+            conexion.commit()
+            return cursor.rowcount
+        except Exception as e:
+            print(f'Error al actualizar cliente: {e}')
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+    @classmethod
+    def eliminar(cls, cliente):
+        conexion = None
+        try:
+            conexion = Conexion().obtener_conexion()
+            cursor = conexion.cursor()
+            valores = (cliente.id,)
+            cursor.execute(cls.ELIMINAR, valores)
+            conexion.commit()
+            return cursor.rowcount
+        except Exception as e:
+            print(f'Error al eliminar cliente: {e}')
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
 if __name__ == '__main__':
 
     # Insertar un nuevo cliente
-    cliente1 = Cliente(nombre='Alejandra', apellido='Tellez', membresia='300')
-    clientes_insertados = ClienteDAO.insertar(cliente1)
-    print(f'Clientes_insertados: {clientes_insertados}')
+    #cliente1 = Cliente(nombre='Alejandra', apellido='Tellez', membresia='300')
+    #clientes_insertados = ClienteDAO.insertar(cliente1)
+    #print(f'Clientes_insertados: {clientes_insertados}')
+
+    # Actualizar un cliente existente
+    #cliente_actualizar = Cliente(id=5, nombre='Alexa', apellido='Tellez', membresia='400')
+    #clientes_actualizados = ClienteDAO.actualizar(cliente_actualizar)
+    #print(f'Clientes_actualizados: {clientes_actualizados}')
+
+    # Eliminar un cliente existente
+    cliente_eliminar = Cliente(id=6)
+    clientes_eliminados = ClienteDAO.eliminar(cliente_eliminar)
+    print(f'Clientes_eliminados: {clientes_eliminados}')
 
     # Seleccionar Clientes
     clientes = ClienteDAO.seleccionar(None)
